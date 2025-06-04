b0f5904a13446b7b02e3f6b56d9760c78afe726b
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-04 07:14:08 +0200

    feat: import Header and render in it in the home function

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import Header from "@/components/layout/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,7 +13,5 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
-  return (
-    <h1>Hello, Next.js!!</h1>
-  );
+  return <Header />;
 }
