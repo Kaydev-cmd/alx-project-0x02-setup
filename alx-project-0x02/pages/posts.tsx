0959f64411ce0c287ac794fa7d29fc66e674499d
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-04 14:30:33 +0200

    feat: import and render Header component

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,7 +1,13 @@
+import Header from "@/components/layout/Header";
 import React from "react";
 
 const posts: React.FC = () => {
-  return <h1>posts</h1>;
+  return (
+    <>
+      <Header />
+      <h1>Posts</h1>
+    </>
+  );
 };
 
 export default posts;
