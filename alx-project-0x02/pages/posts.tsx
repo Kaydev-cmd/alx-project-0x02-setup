b117b53349c9e22c48c50c70e488eda3090ddf2b
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-04 15:47:49 +0200

    feat: fetch posts using getStaticProps from JSONPlaceholder API

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -30,4 +30,15 @@ const posts: React.FC<PostsPageProps> = ({ posts }) => {
   );
 };
 
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
 export default posts;
