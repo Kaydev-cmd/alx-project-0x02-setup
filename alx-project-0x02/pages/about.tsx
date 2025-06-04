c2db4dbd55e2792c88bf122023ff3c0c1cd9cbe1
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-04 13:53:44 +0200

    refactor: refactor Button usage to pass nested styles prop with shape variants

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,17 +1,26 @@
-import Button from "@/components/common/Button";
 import React from "react";
+import Button from "@/components/common/Button";
 
 const about: React.FC = () => {
   return (
     <div className="text-xl font-extralight">
       <h1>About</h1>
       <div className="flex justify-around items-center mt-4">
-        <Button title="Small Rounded-sm" styles="text-sm rounded-sm" />
-        <Button title="Medium Rounded-md" styles="text-base rounded-md" />
-        <Button title="Large Rounded-lg" styles="text-lg rounded-lg" />
         <Button
-          title="Extra-Large Rounded-full"
-          styles="text-xl rounded-full"
+          size="Small Rounded-sm"
+          styles={{ shape: "text-sm rounded-sm" }}
+        />
+        <Button
+          size="Medium Rounded-md"
+          styles={{ shape: "text-base rounded-md" }}
+        />
+        <Button
+          size="Large Rounded-lg"
+          styles={{ shape: "text-lg rounded-lg" }}
+        />
+        <Button
+          size="Extra-Large Rounded-full"
+          styles={{ shape: "text-xl rounded-xl" }}
         />
       </div>
     </div>
