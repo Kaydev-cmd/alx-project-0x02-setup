d2bd64a69fcf482dd922105fe86c635c373d641b
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-04 11:35:54 +0200

    feat: implement basic PostModal component structured with initial stat and props

---
+++ alx-project-0x02/components/common/PostModal.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { PostData, PostModalProps } from "@/interfaces";
+
+const PostModal: React.FC<PostModalProps> = ({ onClose, onSubmit }) => {
+  const [post, setPost] = useState<PostData>({
+    title: "",
+    content: "",
+  });
+  return <div>PostModal</div>;
+};
+
+export default PostModal;
