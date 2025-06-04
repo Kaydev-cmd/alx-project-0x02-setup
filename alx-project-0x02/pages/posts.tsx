51c1c9a490f4fb0bc2b493c72ea2f5411c9d58fa
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-04 15:37:31 +0200

    feat: display list of posts ussing PostCard component in grid layout

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import Header from "@/components/layout/Header";
 import { PostProps, PostData } from "@/interfaces";
+import PostCard from "@/components/common/PostCard";
 
 interface PostsPageProps {
   posts: PostProps[];
 }
 
-const posts: React.FC = () => {
+const posts: React.FC<PostsPageProps> = ({ posts }) => {
   return (
     <>
       <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Posts</h1>
+        </div>
+        <div className="grid grid-cols-3 gap-4">
+          {posts?.map(({ title, content, userId }: PostProps, key: number) => (
+            <PostCard
+              title={title}
+              content={content}
+              userId={userId}
+              key={key}
+            />
+          ))}
+        </div>
+      </main>
     </>
   );
 };
