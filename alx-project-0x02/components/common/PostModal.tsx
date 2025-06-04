f77b1ae77d4afdfb8215da9526d8380b7fddea3b
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-04 11:46:47 +0200

    feat: add content input field to PostModal form

--- alx-project-0x02/components/common/PostModal.tsx
+++ alx-project-0x02/components/common/PostModal.tsx
@@ -41,6 +41,22 @@ const PostModal: React.FC<PostModalProps> = ({ onClose, onSubmit }) => {
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
+          <div className="mb-4">
+            <label
+              htmlFor="content"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Content
+            </label>
+            <input
+              type="text"
+              id="content"
+              name="content"
+              value={post.content}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
         </form>
       </div>
     </div>
