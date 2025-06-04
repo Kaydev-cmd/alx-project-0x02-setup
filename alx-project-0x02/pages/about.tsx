afe6997d36cebea58120a720bd34c390642248f4
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-04 14:30:22 +0200

    feat: import and render Header component

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Button from "@/components/common/Button";
+import Header from "@/components/layout/Header";
 
 const about: React.FC = () => {
   return (
-    <div className="text-xl font-extralight">
-      <h1>About</h1>
-      <div className="flex justify-around items-center mt-4">
-        <Button size="small" styles={{ shape: "rounded-sm" }} />
-        <Button size="medium" styles={{ shape: "rounded-md" }} />
-        <Button size="large" styles={{ shape: "rounded-lg" }} />
+    <>
+      <Header />
+      <div className="text-xl font-extralight">
+        <h1>About</h1>
+        <div className="flex justify-around items-center mt-4">
+          <Button size="small" styles={{ shape: "rounded-sm" }} />
+          <Button size="medium" styles={{ shape: "rounded-md" }} />
+          <Button size="large" styles={{ shape: "rounded-lg" }} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
