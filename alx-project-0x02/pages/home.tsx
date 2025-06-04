a12a090d36ad4310d4c38de48c7492db9408565a
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-04 09:10:30 +0200

    feat: implement card component usage on /home with featured topics section

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Header from "@/components/layout/Header";
+import { CardProps } from "@/interfaces";
+import Card from "@/components/common/Card";
 
 const home: React.FC = () => {
-  return <h1>home</h1>;
+  const cardData: CardProps[] = [
+    {
+      title: "Welcome to SoloForge",
+      content: "A platform where you build, learn, and grow autonomously.",
+    },
+    {
+      title: "Next.js + Tailwind",
+      content: "Master modern web development using the best tools.",
+    },
+    {
+      title: "Reusable Components",
+      content: "Create scalable UI eith component-driven architecture.",
+    },
+  ];
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Featured Topics</h1>
+        </div>
+        <div className="grid grid-cols-3 gap-4">
+          {cardData.map((card, index) => (
+            <Card key={index} title={card.title} content={card.content} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default home;
