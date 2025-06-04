56c4f1c7608d2a7be46638498f63b5342e0fa559
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-04 13:39:52 +0200

    feat: display multiple Button variants with different sizes and border-radius on the About page

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,7 +1,21 @@
+import Button from "@/components/common/Button";
 import React from "react";
 
 const about: React.FC = () => {
-  return <h1>about</h1>;
+  return (
+    <div className="text-xl font-extralight">
+      <h1>About</h1>
+      <div className="flex justify-around items-center mt-4">
+        <Button title="Small Rounded-sm" styles="text-sm rounded-sm" />
+        <Button title="Medium Rounded-md" styles="text-base rounded-md" />
+        <Button title="Large Rounded-lg" styles="text-lg rounded-lg" />
+        <Button
+          title="Extra-Large Rounded-full"
+          styles="text-xl rounded-full"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default about;
