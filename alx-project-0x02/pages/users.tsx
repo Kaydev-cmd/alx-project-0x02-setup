7e365cb010d620b82704545db0f2b069eb31144b
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 09:34:11 +0200

    feat: fetch and provide user data via getStaticProps for Users page

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -49,4 +49,14 @@ const users: React.FC<UsersPageProps> = ({ users }) => {
   );
 };
 
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+  return {
+    props: {
+      users,
+    },
+  };
+}
+
 export default users;
