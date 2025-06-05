d0748805b9191721fcb18f4f4043db42f3c136d2
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 09:17:10 +0200

    chore: add imports for UserCard, UserProps, and Header to users page

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
+import Header from "@/components/layout/Header";
 
 const users: React.FC = () => {
   return <div>users</div>;
