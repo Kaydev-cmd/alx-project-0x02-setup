6b349a374b26b691d43d35fd9dbdd51ff5e5e3b2
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 09:10:48 +0200

    feat: implement UserCard component to display detailed user information

--- alx-project-0x02/components/common/UserCard.tsx
+++ alx-project-0x02/components/common/UserCard.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 import { UserProps } from "@/interfaces";
 
-const UserCard: React.FC<UserProps> = () => {
-  return <div>UserCard</div>;
+const UserCard: React.FC<UserProps> = ({
+  id,
+  name,
+  username,
+  email,
+  address,
+  phone,
+  website,
+  company,
+}) => {
+  return (
+    <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+      <h2 className="text-2xl font-semibold text-gray-800">
+        {name} ({username})
+      </h2>
+      <p className="text-gray-600">Email: {email}</p>
+      <p className="text-gray-600">
+        Address: {address.street}, {address.suite}, {address.city},{" "}
+        {address.zipcode}
+      </p>
+      <p className="text-gray-600">Phone: {phone}</p>
+      <p className="text-gray-600">Website: {website}</p>
+      <p className="text-gray-600">Company: {company.name}</p>
+    </div>
+  );
 };
 
 export default UserCard;
