3f7c7a98a8727db42124a113ef6e8841fae3d644
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 09:25:23 +0200

    feat: render list of users using UserCard component on users page

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -7,8 +7,46 @@ interface UsersPageProps {
   users: UserProps[];
 }
 
-const users: React.FC = () => {
-  return <div>users</div>;
+const users: React.FC<UsersPageProps> = ({ users }) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Users</h1>
+        </div>
+        <div className="grid grid-cols-3 gap-4">
+          {users?.map(
+            (
+              {
+                id,
+                name,
+                username,
+                email,
+                address,
+                phone,
+                website,
+                company,
+              }: UserProps,
+              key: number
+            ) => (
+              <UserCard
+                key={key}
+                name={name}
+                username={username}
+                email={email}
+                address={address}
+                phone={phone}
+                website={website}
+                id={id}
+                company={company}
+              />
+            )
+          )}
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default users;
